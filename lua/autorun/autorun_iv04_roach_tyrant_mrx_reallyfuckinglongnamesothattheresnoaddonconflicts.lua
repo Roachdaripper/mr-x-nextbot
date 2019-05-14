e57e14ab808eac11fb51dda11e49a1180869da36
Roach <roachdaripper1@gmail.com>
2019-05-14 22:46:41 +0100

    autorun file
    remember, no conflicts.

---
+++ lua/autorun/autorun_iv04_roach_tyrant_mrx_reallyfuckinglongnamesothattheresnoaddonconflicts.lua
@@ -0,0 +1,5 @@
+
+sound.AddSoundOverrides("lua/nzombies-unlimited/extensions/nzu_tyrant_mrx_special_enemy/sound/nzu_re2_footsteps_tyrant.lua")
+
+util.PrecacheModel("models/roach_nzu/re2/tyrant.mdl")
+util.PrecacheModel("models/roach/re2/tyrant_hat.mdl")
